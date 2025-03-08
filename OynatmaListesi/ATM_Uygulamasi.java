d881939315fd5e2f707c0f091e840185a3befae5
Abdulsamet Karataş <abdulsametkaratas2841@gmail.com>
2025-03-09 00:33:54 +0300

    Dosya adı değiştirildi

--- OynatmaListesi/ATM_Uygulamasi.java
+++ OynatmaListesi/ATM_Uygulamasi.java
@@ -1,39 +1,36 @@
 import java.util.Scanner;
 
-public class ATM_Uygulamasi{
+public class ATM_Uygulamasi {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int bakiye = 1000,miktar,seçim;
+        int bakiye = 1000,miktar,secim;
         System.out.println("Merhaba JAVA Bank'a hoşgeldiniz");
         System.out.println("Güncel bakiyeniz " + bakiye + " TL");
-        System.out.println("1-) Para Yatır");
-        System.out.println("2-) Para Çek");
+        System.out.println("1-) Para Yatir");
+        System.out.println("2-) Para Cek");
         System.out.println("3-) Bakiye Sorgula");
-        System.out.println("4-) Çıkış yap");
-        while(bakiye > 0){
-            System.out.println("Yapmak istediğiniz işlemi seçin");
-            seçim = scan.nextInt();
-            if((seçim < 1) || (seçim > 4)){
-                System.out.println("Hatalı Tuşlama Tekrar deneyiniz");
+        System.out.println("4-) Cikis yap");
+        while(bakiye >= 0){
+            System.out.println("Yapmak istediginiz işlemi secin");
+            secim = scan.nextInt();
+            if((secim < 1) || (secim > 4)){
+                System.out.println("Hatali Tuslama Tekrar deneyiniz");
                 continue;
             }
-            if(seçim == 1){
-                System.out.println("Yatırılacak tutarı girin");
+            if(secim == 1){
+                System.out.println("Yatirilacak tutari girin");
                 miktar = scan.nextInt();
                 bakiye += miktar;
-                System.out.println("Tutar Yatırıldı!");
-                continue;
-            }else if(seçim == 2){
-                System.out.println("Çekmek istediğiniz tutarı giriniz");
+                System.out.println("Tutar Yatirildi!");
+            }else if(secim == 2){
+                System.out.println("Cekmek istediginiz tutari giriniz");
                 miktar = scan.nextInt();
                 bakiye -= miktar;
-                System.out.println("Tutar Çekildi!");
-                continue;
-            }else if(seçim == 3){
-                System.out.println("Güncel bakiyeniz " + bakiye + "TL");
-                continue;
-            } else if (seçim == 4) {
-                System.out.println("Çıkış yapılıyor...");
+                System.out.println("Tutar Cekildi!");
+            }else if(secim == 3){
+                System.out.println("Guncel bakiyeniz " + bakiye + "TL");
+            } else {
+                System.out.println("Cikis yapiliyor...");
                 break;
             }
         }
